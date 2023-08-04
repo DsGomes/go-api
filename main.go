a7644bbfa638a0662bebb69fd4b732b6a78d2d11
DsGomes <dsgomes1990@gmail.com>
2023-08-04 19:11:49 -0300

    feat(main): create server

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/dsgomes/rest-api/configs"
+	"github.com/dsgomes/rest-api/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	err := configs.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+	r.Post("/", handlers.Create)
+	r.Put("/{id}", handlers.Update)
+	r.Delete("/{id}", handlers.Delete)
+	r.Get("/", handlers.List)
+	r.Get("/{id}", handlers.Get)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
 }
