5f6b2020ecd6bd05d98a6f3b8ab1cc23c9d51434
DsGomes <dsgomes1990@gmail.com>
2023-08-04 19:04:35 -0300

    feat(handler): add get by id handler

---
+++ handlers/get.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/dsgomes/rest-api/models"
+)
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Id parser error: %v", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	todo, err := models.Get(int64(id))
+	if err != nil {
+		log.Printf("Update error: %v", err)
+		http.Error(
+			w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todo)
+}
