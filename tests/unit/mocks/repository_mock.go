517b113794d47d7583081cd9d03fa496f2d16c6d
DsGomes <dsgomes1990@gmail.com>
2023-08-22 22:22:15 -0300

    chore(mocks): refactor tests and extracting mocks to another folder

---
+++ tests/unit/mocks/repository_mock.go
@@ -0,0 +1,35 @@
+package mocks
+
+import (
+	"github.com/dsgomes/rest-api/internal/core/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockTodoRepository struct {
+	mock.Mock
+}
+
+func (m *MockTodoRepository) GetAll() ([]domain.Todo, error) {
+	args := m.Called()
+	return args.Get(0).([]domain.Todo), args.Error(1)
+}
+
+func (m *MockTodoRepository) Get(id string) (domain.Todo, error) {
+	args := m.Called(id)
+	return args.Get(0).(domain.Todo), args.Error(1)
+}
+
+func (m *MockTodoRepository) Insert(todo *domain.Todo) (int64, error) {
+	args := m.Called(todo)
+	return args.Get(0).(int64), args.Error(1)
+}
+
+func (m *MockTodoRepository) Update(id string, todo *domain.Todo) (int64, error) {
+	args := m.Called(id)
+	return args.Get(0).(int64), args.Error(1)
+}
+
+func (m *MockTodoRepository) Delete(id string) (int64, error) {
+	args := m.Called(id)
+	return args.Get(0).(int64), args.Error(1)
+}
--- tests/unit/usecases/todo_test.go
+++ tests/unit/usecases/todo_test.go
@@ -6,38 +6,9 @@ import (
 
 	"github.com/dsgomes/rest-api/internal/core/domain"
 	"github.com/dsgomes/rest-api/internal/core/usecases"
+	"github.com/dsgomes/rest-api/tests/unit/mocks"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
-
-type MockTodoRepository struct {
-	mock.Mock
-}
-
-func (m *MockTodoRepository) GetAll() ([]domain.Todo, error) {
-	args := m.Called()
-	return args.Get(0).([]domain.Todo), args.Error(1)
-}
-
-func (m *MockTodoRepository) Get(id string) (domain.Todo, error) {
-	args := m.Called(id)
-	return args.Get(0).(domain.Todo), args.Error(1)
-}
-
-func (m *MockTodoRepository) Insert(todo *domain.Todo) (int64, error) {
-	args := m.Called(todo)
-	return args.Get(0).(int64), args.Error(1)
-}
-
-func (m *MockTodoRepository) Update(id string, todo *domain.Todo) (int64, error) {
-	args := m.Called(id)
-	return args.Get(0).(int64), args.Error(1)
-}
-
-func (m *MockTodoRepository) Delete(id string) (int64, error) {
-	args := m.Called(id)
-	return args.Get(0).(int64), args.Error(1)
-}
 
 var mockTodos = []domain.Todo{
 	{
@@ -59,7 +30,7 @@ var mockTodos = []domain.Todo{
 }
 
 func TestGetAll(t *testing.T) {
-	mockRepo := new(MockTodoRepository)
+	mockRepo := new(mocks.MockTodoRepository)
 	mockRepo.On("GetAll").Return(mockTodos, nil)
 	useCase := usecases.NewTodoUseCase(mockRepo)
 
@@ -72,7 +43,7 @@ func TestGetAll(t *testing.T) {
 }
 
 func TestGet(t *testing.T) {
-	mockRepo := new(MockTodoRepository)
+	mockRepo := new(mocks.MockTodoRepository)
 	mockRepo.On("Get", "ABC").Return(mockTodos[0], nil)
 	useCase := usecases.NewTodoUseCase(mockRepo)
 
